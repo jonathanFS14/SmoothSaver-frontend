60d73bc50fe858b41c416913440c6e261a3e4904
NikoDatamand <niki081b@stud.kea.dk>
2023-11-08 13:06:47 +0100

    Updated index html, styling and js

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 import { API_URL } from "../../../settings.js"
 const URL = API_URL + "/salling"
-import {sanitizeStringWithTableRows, makeOptionsToken, handleHttpErrors} from "../../../utils.js"
+import { sanitizeStringWithTableRows, makeOptionsToken, handleHttpErrors } from "../../../utils.js"
 
-
-document.getElementById("getAllStoresByZip").addEventListener("click", initGetAllStoresByZip)
+document.getElementById("zip-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    initGetAllStoresByZip();
+});
 
 async function initGetAllStoresByZip() {
     const zip = document.getElementById("zip").value;
     const options = makeOptionsToken("GET", null, false);
-    const stores = await fetch(URL + "/" + zip, options).then(res => res.json())
+    const stores = await fetch(URL + "/" + zip, options).then(res => res.json());
 
-    const tableRows = stores.map(store => 
+    const tableRows = stores.map(store =>
         `<tr>
             <td>${store.store.address.street}</td>
             <td>${store.store.brand}</td>
         </tr>`
-    )
-    const tableRowsAsStr = tableRows.join("")
+    );
+
+    const tableRowsAsStr = tableRows.join("");
     document.getElementById("tbl-body").innerHTML = sanitizeStringWithTableRows(tableRowsAsStr);
 }
-
