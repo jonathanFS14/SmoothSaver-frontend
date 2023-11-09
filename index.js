24cfa85edf5e0c0d4dc7bc58e5e5bf176a8d610b
Jonathan Finner Smith <jona08p8@stud.kea.dk>
2023-11-09 21:02:28 +0100

    aaa

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ document.getElementById("cards-grid").onclick = (evt) => {
 
   const storeId = evt.target.id;
   console.log(storeId);
-  document.getElementById("msg").innerText = storeId;
+  if(storeId === "cards-grid") {
+    return;
+  }
+  initGetStoreById(storeId);
 }
 
 function getStoreImage(storeName) {
@@ -43,28 +46,23 @@ async function initGetAllStoresByZip() {
     if (json.length === 0) {
       document.getElementById("error").innerHTML = "Ingen butikker fundet, indtast et andet postnummer";
       }
-    const tableRows = json.map(storeData =>{
+    const cards = json.map(storeData => {
       const openTime = new Date(storeData.store.hours[0].open + 'Z').toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
       const closeTime = new Date(storeData.store.hours[0].close + 'Z').toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
       return `
-      <div id="${storeData.store.id}">
- 
         <div id="${storeData.store.id}" class="card" style="width: 18rem;">
-        <img class="card-img-top" width="40px" length="40px" src="${getStoreImage(storeData.store.name)}" alt="">
-        <div class="card-body">
-        <h5 class="card-title">${storeData.store.name}</h5>
-        <p class="card-text">${storeData.store.address.street}</p>
-        <p class="card-text">åbningstid: ${openTime}</p>
-        <p class="card-text">butikken lukker: ${closeTime}</p>
-        <p class="card-text"> store id: ${storeData.store.id}</p>
+        <img id="${storeData.store.id}" class="card-img-top" width="40px" length="40px" src="${getStoreImage(storeData.store.name)}" alt="">
+        <div id="${storeData.store.id}" class="card-body">
+        <h5 id="${storeData.store.id}" class="card-title">${storeData.store.name}</h5>
+        <p id="${storeData.store.id}" class="card-text">${storeData.store.address.street}</p>
+        <p id="${storeData.store.id}" class="card-text">åbningstid: ${openTime}</p>
+        <p id="${storeData.store.id}" class="card-text">butikken lukker: ${closeTime}</p>
       </div>
-    </div>
-    
     </div>
       `
   });
-    const tableRowsAsStr = tableRows.join("");
-    document.getElementById("cards-grid").innerHTML = tableRowsAsStr;
+    const cardsAsStr = cards.join("");
+    document.getElementById("cards-grid").innerHTML = cardsAsStr;
   } catch (error) {
     document.getElementById("error").innerHTML = error;
     console.error('Could not fetch the data: ', error);
@@ -72,8 +70,35 @@ async function initGetAllStoresByZip() {
     spinner.style.display = "none";
   }
 
+}
 
-
+  async function  initGetStoreById(storeId) {
+    try {
+      const response = await fetch(URL + "/id/" + storeId);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const json = await response.json();
+     const ingredients = json.map(ingredient => 
+      ingredient.clearances.array.forEach(clearances => {
+        `
+        <tr>
+        <td> produkt: ${clearances.product.description}</td>
+        <td> original pris: ${clearances.offer.originalPrice}</td>
+        <td> tilbuds pris: ${clearances.offer.originalPrice}</td>
+        </tr>
+        `
+      })
+     
+     )
+     const ingredientssAsStr = ingredients.join("");
+     document.getElementById("ingredients").innerHTML = ingredientssAsStr;
+    } catch (error) {
+      document.getElementById("error").innerHTML = error;
+      console.error('Could not fetch the data: ', error);
+    }finally {
+     
+    }
 }
 
 
