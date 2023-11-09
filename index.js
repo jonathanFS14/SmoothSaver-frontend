799e53b1446f60ce9deaafe7b650eec423067894
Jonathan Finner Smith <jona08p8@stud.kea.dk>
2023-11-09 09:38:01 +0100

    slettet gammelt kode

--- index.js
+++ index.js
@@ -5,48 +5,14 @@ const URL = API_URL + "/salling"
 
 document.getElementById("zip-form").addEventListener("submit", function (event) {
     event.preventDefault();
-    initGetAllStoresByZiptest();
+    initGetAllStoresByZip();
 });
 
 async function initGetAllStoresByZip() {
   document.getElementById("error").innerHTML = "";
-  document.getElementById("tbl-body").innerHTML = "";
+  document.getElementById("cards-grid").innerHTML = "";
   const zip = document.getElementById("zip").value;
   const spinner = document.getElementById('spinner');
-  
-  try {
-    spinner.style.display = "block";
-    const response = await fetch(URL + "/" + zip);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const json = await response.json();
-    if (json.length === 0) {
-      document.getElementById("error").innerHTML = "Ingen butikker fundet, indtast et andet postnummer";
-      }
-    const tableRows = json.map(storeData => 
-      `<tr>
-          <td>${storeData.store.name}</td>
-          <td>${storeData.store.address.street}</td>
-          <td>${storeData.store.brand}</td>
-      </tr>`
-    );
-    const tableRowsAsStr = tableRows.join("");
-    document.getElementById("tbl-body").innerHTML = tableRowsAsStr;
-  } catch (error) {
-    document.getElementById("error").innerHTML = error;
-    console.error('Could not fetch the data: ', error);
-  }finally {
-    spinner.style.display = "none";
-  }
-}
-
-async function initGetAllStoresByZiptest() {
-  document.getElementById("error").innerHTML = "";
-  document.getElementById("tbl-body").innerHTML = "";
-  const zip = document.getElementById("zip").value;
-  const spinner = document.getElementById('spinner');
-  
   try {
     spinner.style.display = "block";
     const response = await fetch(URL + "/" + zip);
