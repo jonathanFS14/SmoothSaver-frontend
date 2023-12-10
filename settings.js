ce519f4f37812430bdbe6557dc0c984389a22e24
Jonathan Finner Smith <113142810+jonathanFS14@users.noreply.github.com>
2023-12-10 11:36:01 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://smoothsaverbackend.azurewebsites.net/api"
+export const API_URL = "https://smoothsaver-backend.azurewebsites.net/api"
 //export const API_URL = "http://localhost:8080/api"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)";
